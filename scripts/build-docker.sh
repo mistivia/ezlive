1929cd6231a38a4bcd8950417a038ec35c60b54d
Mistivia <i@mistivia.com>
2025-11-05 13:45:07 +0800

    rename readme

--- scripts/build-docker.sh
+++ scripts/build-docker.sh
@@ -5,6 +5,6 @@ mkdir rootfs/etc/ezlive/
 sh ./scripts/create_rootfs.sh ezlive rootfs/
 cp ezlive rootfs/
 cp -L -r /etc/ssl rootfs/etc/
-sudo docker build -t ezlive .
+sudo docker build -t localhost/ezlive .
 sudo docker save localhost/ezlive | gzip > ezlive-docker-image.tar.gz
 sudo docker rmi localhost/ezlive
