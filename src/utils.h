1cd8b97aded5f0f3f2d8cc4b7d4ab4f5157e51f1
Mistivia <i@mistivia.com>
2026-03-03 19:17:28 +0800

    add defer

--- src/utils.h
+++ src/utils.h
@@ -1,6 +1,24 @@
 #pragma once
 
+#include <functional>
+
 namespace ezlive {
+
+template<typename F>
+class defer {
+public:
+    explicit defer(F f) : m_func(f), m_active(true) {}
+    ~defer() { if (m_active) { m_func(); } }
+    defer(const defer&) = delete;
+    defer& operator=(const defer&) = delete;
+    defer(defer&& other) : m_func(std::move(other.m_func)), m_active(other.m_active) {
+        other.m_active = false;
+    }
+    void cancel() { m_active = false; }
+private:
+    F m_func;
+    bool m_active;
+};
 
 void tmp_local_filename(const char *prefix, char *buf);
 
